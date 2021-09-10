831e26f08fb00fed3a6c0d00e6e294b65d2530df
Mistywu <73806658+Mistywu@users.noreply.github.com>
2021-09-10 23:18:44 +0800

    September 10, 2021
    add ColorMap

---
+++ Mesh_Platform9.10/Algorithm/SurfaceMesh_error.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <easy3d/core/surface_mesh.h>
+
+namespace easy3d
+{
+	class SurfaceMesh;
+}
+using namespace easy3d;
+class SurfaceMesh_error
+{
+public:
+	SurfaceMesh_error(SurfaceMesh *mesh);
+	~SurfaceMesh_error();
+
+	void mean_error(SurfaceMesh *original_mesh);
+	void RMS_error(SurfaceMesh *original_mesh);
+	void Hausdorff_error(SurfaceMesh *original_mesh);
+
+private:
+	SurfaceMesh *mesh_;
+	SurfaceMesh::FaceProperty<vec3>   fnormal;
+
+};
+
---
+++ Mesh_Platform9.10/Dialog/WidgetRendering.h
@@ -0,0 +1,53 @@
+
+#ifndef WIDGET_RERNDERING_H
+#define WIDGET_RERNDERING_H
+
+#include <QtWidgets/QWidget>
+#include "ui_Widget_Rendering.h"
+
+//class Mesh_PlatformClass;
+class ViewerQt;
+class Mesh_PlatformClass;
+
+namespace Ui {
+	class WidgetRendering;
+}
+
+
+class WidgetRendering : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit WidgetRendering(QWidget *parent);
+	//explicit WidgetRendering(Mesh_PlatformClass *window);
+
+	~WidgetRendering() override;
+
+	// update the panel to be consistent with the drawable's rendering parameters
+	//void updatePanel() override;
+protected:
+
+	ViewerQt *viewer_;
+	Mesh_PlatformClass *window_;
+
+
+public slots:
+	
+	void setPointVisible(bool b);
+	void setEdgeVisible(bool b);
+	//void setEdgeWidth();
+	void setTriangleVisible(bool b);
+	void setFaceNormalVisible(bool b);
+	//void setinputModelVisible();
+	void original_model();
+	void result_model();
+	void clear_model();
+private:
+	Ui::WidgetRendering *ui;
+	//void updateNormalFieldBuffer(easy3d::Model *model, const std::string &name);
+
+
+};
+
+#endif // WIDGET_DRAWABLE_LINES_H
