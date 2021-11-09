418f28c9e3655ca10a6c035c8e2b19f9c015459f
Mistywu <73806658+Mistywu@users.noreply.github.com>
2021-11-09 21:52:10 +0800

    Add files via upload

---
+++ Mesh_Platform 11.09/Dialog/DialogMSAT_View.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "ui_Dialog_MSAT.h"
+#include <QDialog>
+
+class Mesh_PlatformClass;
+class ViewerQt;
+namespace easy3d
+{
+	class Model;
+}
+
+class DialogMSAT_View : public QDialog, public Ui::DialogMSAT
+{
+	Q_OBJECT
+
+public:
+	explicit DialogMSAT_View(Mesh_PlatformClass *window);
+	~DialogMSAT_View();
+
+
+protected:
+
+	ViewerQt *viewer_;
+	Mesh_PlatformClass *window_;
+	easy3d::Model* copyorigin;
+	easy3d::Model* copysecond;
+
+private Q_SLOTS:
+	void apply_MSAT();
+	void setShowFeatureEdges();
+	void setShowFeaturevertices();
+	void apply_FeatureGraph();
+
+private:
+	void updateVectorFieldBuffer(easy3d::Model *model, const std::string &name);
+	void updateVertexFieldBuffer(easy3d::Model *model, const std::string &name);
+
+
+
+};
+
